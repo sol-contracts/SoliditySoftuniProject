b69c3dce11bf958a5cf0bbb453411301e7596e82
Vladislav <budnikov@abv.bg>
2018-03-15 17:14:36 +0200

    Implementing IMarketplace interface methods

--- marketplace.sol
+++ marketplace.sol
@@ -1,7 +1,4 @@
-
 pragma solidity ^0.4.21;
-
-
 
 contract IMarketplace {
     function buy(bytes32 ID, uint quantity) public payable {}
@@ -19,5 +16,44 @@ contract IMarketplace {
 }
 
 contract Marketplace is IMarketplace {
+    struct Product{
+        string name;
+        uint price;
+        uint quantity;
+    }
+    mapping(bytes32=>Product) products;
+    bytes32[] productIndeces;
     function Marketplace() public {}
+    
+    function buy(bytes32 ID, uint quantity) public payable {
+        ReduceProductQuantity(ID, quantity);
+    }
+    
+    function update(bytes32 ID, uint newQuantity) public {
+        products[ID].quantity = newQuantity;
+    }
+
+     //creates a new product and returns its ID
+    function newProduct(string name, uint price, uint quantity) public returns(bytes32) {
+        bytes32 productId = keccak256(name);
+        products[productId] = Product(name, price, quantity);
+        productIndeces.push(productId);
+        return productId;
+    }
+    
+    function getProduct(bytes32 ID) public view returns(string name, uint price, uint quantity) {
+        
+    }
+
+    function getProducts() public view returns(bytes32[]) {
+         return productIndeces;
+    }
+    
+    function getPrice(bytes32 ID, uint quantity) public view returns (uint) {
+        return products[ID].price;
+    }
+    
+    function ReduceProductQuantity(bytes32 ID, uint quantity) internal{
+        products[ID].quantity-=quantity;
+    }
 }
